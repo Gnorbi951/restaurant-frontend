e8aa1104484ad0d8f83977a166dcf57afb689a92
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-01 20:37:29 +0200

    Adjust navbar css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
           <NavBarContainer>
             <NavBarLink to={'/'}>Home</NavBarLink>
             <NavBarLink to={'/'}>Menu</NavBarLink>
-            <NavBarLink to={'/'}>Order Now</NavBarLink>
+            <NavBarLink to={'/'}>Order&nbsp;Now</NavBarLink>
             <NavBarLink to={'/'}>About</NavBarLink>
             <LoginIcon to={'/'}>Login</LoginIcon>
           </NavBarContainer>
@@ -19,26 +19,37 @@ const NavBar = () => {
 
 }
 
-const LoginIcon = styled.div`
+const LoginIcon = styled(Link)`
     color: #fff;
     text-decoration: none;
     margin: 0.5rem 1rem;
+    font-size: 1.8rem;
+    color: #fcbf49;
     font-weight: bold;
     margin-left: auto;
+    &:hover {
+    transition: 350ms;
+    color: #f77f00;
+  }
 `;
 
 const NavBarLink = styled(Link)`
-    color: #fff;
+    color: #fcbf49;
     text-decoration: none;
     margin: 0.5rem 1rem;
     font-weight: bold;
+    font-size: 1.8rem;
+    &:hover {
+    transition: 350ms;
+    color: #f77f00;
+    justify-content: flex-start;
+  }
 `;
 
 const NavBarContainer = styled.div`
     background-color: #003049;
     display: flex;
-    width: 100vw;
-    flex-direction: row;
+    width: 100%;
 `;
 
 export default NavBar;
