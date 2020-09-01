88408e74fa25828fb76ccbe296f9ed70ac2e67e3
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-01 20:39:05 +0200

    Remove clutter

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -3,20 +3,10 @@ import axios from "axios";
 
 
 const MainPage = () => {
-    const [message, setMessage] = useState();
-
-    useEffect(() => {
-        axios.get("/test")
-            .then(response => {
-                setMessage(response)
-                console.log(response)
-            })
-    }, [])
 
     return(
         <React.Fragment>
-            <h1>The server responded:</h1>
-            {message ? <p>{message.data.message}</p> : <p>Loading</p>}
+            <h1>Placeholder</h1>
         </React.Fragment>
     )
 }
