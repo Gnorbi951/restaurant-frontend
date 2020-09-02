30736b1724f18068d891b66cef5bfeef57793449
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-02 12:06:12 +0200

    Add image and center it with Title

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,14 +1,39 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
+import background from '../images/restaurant_background.jpg'
+import styled from 'styled-components';
 
 
 const MainPage = () => {
 
     return(
         <React.Fragment>
-            <h1>Placeholder</h1>
+            <WelcomeImageContainer>
+                <Title>Margheriti</Title>
+                <h1>The Best Restaurant in your area</h1>
+                <RestaurantImage src={background} alt={"background_image"} />
+            </WelcomeImageContainer>
         </React.Fragment>
     )
 }
+
+const WelcomeImageContainer = styled.div`
+    position: relative;
+    text-align: center;
+    width: 100%;
+    height: 100%;
+`;
+
+const Title = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+
+const RestaurantImage = styled.img`
+    width:100%;
+    height: 100%;
+    
+`;
 
 export default MainPage;
