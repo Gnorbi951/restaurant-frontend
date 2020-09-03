6b3f5faa3576c33e4d35e6d158790bafdabf00a0
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-03 16:15:50 +0200

    Add Menu title

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import styled from 'styled-components';
 const Menu = () => {
     return(
-        <h1>Menu</h1>
+        <Header>Menu</Header>
     );
 }
+
+const Header = styled.div`
+    font-family: 'Leckerli One', Helvetica, sans-serif;
+    font-size: 8vw;
+    margin-top: 4vw;
+    display: block;
+    text-align: center;
+`;
 
 export default Menu;
