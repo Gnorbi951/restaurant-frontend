b2616a2bf4edef43d8908355dad025ce18f19f57
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-10 12:21:32 +0200

    Find box elements to display picture

--- src/Components/SubComponents/MenuItems.js
+++ src/Components/SubComponents/MenuItems.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
 import cameraIcon from "../../images/camera.png"
 
 const MenuItems = (props) => {
+
+    useEffect(() => {
+        setup()
+    }, )
+    function setup() {
+        let images = document.getElementsByClassName("logo");
+        for (let image of images) {
+            let imageId = image.id.substr(4);
+            let box = document.getElementById(`box${imageId}`);
+            console.log(box)
+        }
+    }
+
     return(
         <React.Fragment>
 
@@ -18,7 +31,12 @@ const MenuItems = (props) => {
                             <tbody>
                             {props.items.map((item) => (
                                 <tr key={item.id}>
-                                <Data><a href={`${item.picture}`}><Logo src={cameraIcon} alt={"camera_icon"} /></a></Data>
+                                <Data>
+                                    <a id={`link${item.id}`} className={"logo"} href={`${item.picture}`}>
+                                        <Logo src={cameraIcon} alt={"camera_icon"} />
+                                    </a>
+                                    <Box id={`box${item.id}`}>Some Text</Box>
+                                </Data>
                                 <Data>{item.name}</Data>
                                 <Data>{item.price} Ft</Data>
                                 </tr>
@@ -38,6 +56,15 @@ const Data = styled.td`
 const Logo = styled.img`
     width: 4vw;
     padding-left: auto;
+`;
+
+const Box = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    border: solid 1px;
+    background-color: #fff;
+    padding: 5px;
 `;
 
 
