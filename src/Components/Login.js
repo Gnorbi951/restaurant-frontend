e6d925d99c214721d5d33839d7cdce43ab24099d
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-14 19:28:51 +0200

    Add style to login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -6,7 +6,7 @@ import Cookies from 'universal-cookie';
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("No message");
+    const [message, setMessage] = useState("");
     const cookies = new Cookies();
     const [isLoggedIn, setLoggedIn] = useState(false);
 
@@ -51,38 +51,108 @@ const Login = () => {
 
     return(
         <React.Fragment>
-            <input
+            <LoginContainer>
+                <strong>Username:</strong>
+            <Input
                 type = "text"
                 name = "username"
                 autoComplete={"off"}
-                placeholder={"Username"}
+                placeholder={"Enter Username"}
                 value = {username}
                 onChange = {e =>
                     setUsername(e.target.value)
                 }
             />
-            <input
+                <strong>Password:</strong>
+            <Input
                 type = "password"
                 name = "password"
-                placeholder={"password"}
+                placeholder={"Enter password"}
                 value = {password}
                 onChange = {e =>
                     setPassword(e.target.value)
                 }
             />
-            <button
+            <Button
                 type = "submit"
                 onClick = {login} >
                 Login
-                </button>
-            {isLoggedIn ? (<button
+                </Button><br />
+            {isLoggedIn ? (<LogoutButton
                 type="submit"
                 onClick= {logout} >
                 Logout
-            </button>) : ("")}
-                        <div> {message} </div>
+            </LogoutButton>) : ("")}
+                        <MessageContainer> {message} </MessageContainer>
+            </LoginContainer>
         </React.Fragment>
     );
 }
+
+const LoginContainer = styled.div`
+    width: 40%;
+    padding: 2vw;
+    margin: auto;
+    @media (max-width: 600px) {
+        width: 90%;
+    }
+`;
+
+const Input = styled.input`
+    width: 100%;
+    padding-top: 1vw;
+    padding-bottom: 1vw;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+
+`;
+
+const Button = styled.button`
+    background-color: #141414;
+    color: #fcbf49;
+    &:hover {
+        transition: 350ms;
+        color: #f77f00;
+    }
+    padding: 1vw;
+    margin: 1.3vw 0;
+    border: none;
+    cursor: pointer;
+    width: 50%;
+    @media (max-width: 600px) {
+        width: 30%;
+        font-size: 5vw;
+    }
+    font-size: 3vw;
+    font-weight: bold;
+    border-radius: 5px;
+`;
+
+const LogoutButton = styled.button`
+    width: auto;
+    padding: 0.8vw;
+    background-color: #141414;
+    font-weight: bold;
+    cursor: pointer;
+    border: none;
+    border-radius: 3px;
+    font-size: 2vw;
+    color: #fcbf49;
+    @media (max-width: 600px) {
+        font-size: 2vw;
+    }
+    &:hover {
+        transition: 350ms;
+        color: #f77f00;
+    }
+`;
+
+const MessageContainer = styled.div`
+   font-size: 4vw;
+   font-weight: bold;
+   color: #141414;
+`;
 
 export default Login;
