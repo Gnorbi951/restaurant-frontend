a9c5df4d7e8ec7aa8ffc4fa473bad2b3d50db6f2
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-15 15:35:08 +0200

    Extend order component

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 const Order = () => {
+    const [menu, setMenu] = useState([]);
+    const config = {
+        // Send cookie token
+        withCredentials: true
+    };
 
-    return(
-        <React.Fragment>
-            Order
-        </React.Fragment>
-    );
+    useEffect(() => {
+        console.log(localStorage.getItem("name"))
+        axios.get("/order/all", config)
+            .then(resp => {
+                console.log(resp.data)
+                setMenu(resp.data)
+            })
+    }, [])
+
+    if (localStorage.getItem("name") === null) {
+        return(
+            <React.Fragment>
+                You must log in to order
+            </React.Fragment>
+        )
+    } else {
+        return(
+            <React.Fragment>
+                Order
+            </React.Fragment>
+        );
+    }
+
 }
 
 export default Order;
