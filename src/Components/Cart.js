1074f77642ee479ab75dcda193bad72183682fcf
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-17 16:20:00 +0200

    Add basic cart, thats not functional yet

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -11,11 +11,83 @@ const Cart = () => {
     return(
         <React.Fragment>
             <CartContainer>
-            <BackButton to={"/order_now"}>Back</BackButton>
+                <BackButton to={"/order_now"}>Back</BackButton>
+                <ShoppingCart>
+                    <Title>
+                        Shopping Bag
+                    </Title>
+                    <Item>
+                        <div className="buttons">
+                            <CartButton>X</CartButton>
+                        </div>
+
+                        <div className="image">
+                            <img src="item-1.png" alt=""/>
+                        </div>
+
+                        <div className="description">
+                            <span>Order Item</span>
+                        </div>
+
+                        <div className="quantity">
+                            <CartButton className="plus-btn" type="button" name="button">
+                                +
+                            </CartButton>
+                            <input type="text" name="name" value="1" />
+                                <CartButton className="minus-btn" type="button" name="button">
+                                    -
+                                </CartButton>
+                        </div>
+                        <div className="total-price">$549</div>
+                    </Item>
+                </ShoppingCart>
             </CartContainer>
         </React.Fragment>
     )
 }
+
+const Quantity = styled.div`
+  border: none;
+  text-align: center;
+  width: 32px;
+  font-size: 16px;
+  color: #43484D;
+  font-weight: 300;
+`;
+
+const CartButton = styled.button`
+    position: relative;
+    padding: 5px;
+    margin-right: 30px;
+    margin-left 30px;
+`;
+
+const Item = styled.div`
+    padding: 20px 30px;
+    height: 120px;
+    display: flex;
+`;
+
+const Title = styled.div`
+    height: 60px;
+    border-bottom: 1px solid #E1E8EE;
+    padding: 20px 30px;
+    color: #5E6977;
+    font-size: 18px;
+    font-weight: 400;
+`;
+
+const ShoppingCart = styled.div`
+    width: 750px;
+    height: 423px;
+    margin: 80px auto;
+    background: #FFFFFF;
+    box-shadow: 1px 2px 3px 0px rgba(0,0,0,0.10);
+    border-radius: 6px;
+    
+    display: flex;
+    flex-direction: column;
+`;
 
 const CartContainer = styled.div`
     margin-top: 3vw;
