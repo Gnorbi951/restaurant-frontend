c5eae2d473b990f9ae726b667dade941ae1359f1
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-17 21:33:22 +0200

    Reconfigure axios will be used in a future version

--- src/Components/Order.js
+++ src/Components/Order.js
@@ -10,7 +10,7 @@ const Order = () => {
     const [cart, setCart] = useContext(CartContext);
 
     useEffect(() => {
-        axios.get("/order/all", {withCredentials: true})
+        axios.get("/order/all")
             .then(resp => {
                 setMenu(resp.data)
             })
