d0deed63bcf63dffc6cdfe22a6c185e6071cd822
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-17 15:03:29 +0200

    Add back button

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,12 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 
 const Cart = () => {
 
     return(
         <React.Fragment>
-            This is the cart
+            <CartContainer>
+            <BackButton href={"/order_now"}>Back</BackButton>
+            </CartContainer>
         </React.Fragment>
     )
 }
+
+const CartContainer = styled.div`
+    margin-top: 3vw;
+    padding: 1vw;
+`;
+
+const BackButton = styled.a`
+    margin-top: 200px;
+    width: auto;
+    padding: 1vw;
+    background-color: #141414;
+    font-weight: bold;
+    cursor: pointer;
+    border: none;
+    border-radius: 3px;
+    font-size: 2rem;
+    color: #fcbf49;
+    @media (max-width: 600px) {
+        font-size: 1rem;
+    }
+    &:hover {
+        transition: 350ms;
+        color: #f77f00;
+    }
+    text-decoration: none;
+`;
 
 export default Cart
