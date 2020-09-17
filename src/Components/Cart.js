bbec2f86013404bc94c814134d9340743c99e39d
Gőcze Norbert <gnorbi951@gmail.com>
2020-09-17 14:52:45 +0200

    Add Cart Component

---
+++ src/Components/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cart = () => {
+
+    return(
+        <React.Fragment>
+            This is the cart
+        </React.Fragment>
+    )
+}
+
+export default Cart
